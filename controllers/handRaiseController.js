b84c611501823167d9e7be82174475e6c9cded17
Raghav <raghav2004kaushal@gmail.com>
2025-09-15 09:37:09 +0530

    modifications in handraise controller

--- controllers/handRaiseController.js
+++ controllers/handRaiseController.js
@@ -9,7 +9,9 @@ const raiseHand = async(req,res,next) => {
         }
 
         const hand = await HandRaise.create({user: req.user._id});
-        res.status(200).json({message: "Hand Raised", hand});
+        await hand.populate("user","name")
+
+        res.status(200).json({message: `Hand Raised ${hand.user.name}`, hand});
     }catch(err){
         next(err);
     }
@@ -18,11 +20,14 @@ const raiseHand = async(req,res,next) => {
 const lowerHand = async (req,res,next) => {
     try{
         const deletedHand = await HandRaise.findOneAndDelete({user : req.user._id});
+        await deletedHand.populate("user" , "name")
         if(!deletedHand){
-            return res.status(400).json({message : "Hand not raised"});
+            return res.status(400).json({
+                message : 'Hand not raised'
+            });
         }
-
-        res.status(200).json({message : "Hand lowered"});
+    
+        res.status(200).json({message : `Hand lowered ${deletedHand.user.name}`});
     }catch (err){
         next(err);
     }
